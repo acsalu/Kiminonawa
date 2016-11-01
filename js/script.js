9b399fc748868b614fa9679aa7979b844f230725
Jean Lin <hsiaoching.jean@gmail.com>
2016-10-31 21:37:49 -0400

    URL Sanitize

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $(function() {
 	// Can access page DOM here
+    var baseUrl = getBaseUrl(location.href);
 });
+
+function getBaseUrl(origUrl) {
+    var pathArray = origUrl.split('/');
+    var protocol = pathArray[0];
+    var host = pathArray[2];
+    return protocol + '//' + host;
+}
