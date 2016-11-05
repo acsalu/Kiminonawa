e7a3e3331eb79a007996ac4e5d590ab60f488f2f
shouponlin <shoupon.lin@gmail.com>
2016-11-05 14:35:57 -0400

    Case insensative matching and more accurate regex

--- js/script.js
+++ js/script.js
@@ -1,40 +1,43 @@
 var parser = {};
 // Return a list containing the DOM path leading to the html object containing
 // the phrase 'Taiwan'.
-parser.getTaiwanNode = function() {
-  // TODO(Dada): Make this case insensitive.
-  return $('*:contains("Taiwan")');
+parser.getOffendingNode = function() {
+  var taiwanNode = parser.getTaiwanNode_();
+  if (taiwanNode.length == 0) {
+    return null;
+  }
+  if (parser.matchOffending_(parser.getText_(taiwanNode))) {
+    return taiwanNode;
+  }
+  return null;
 };
-parser.getText = function(node) {
+
+// Private methods.
+parser.getTaiwanNode_ = function() {
+  var node = $('*:contains("Taiwan")');
+  if (node) {
+    return node;
+  }
+  return $('*:contains("taiwan")');
+};
+parser.getText_ = function(node) {
   var lastNode = node.last();
   return lastNode[0].innerHTML;
 };
-parser.patterns = [
-  // TODO(Dada): Need finer matching regex here.
-  new RegExp('taiwan.*province.*of.*china'),
-  new RegExp('taiwan.*prc'),
-  new RegExp('taiwan.*china'),
-  new RegExp('china.*taiwan')
+parser.patterns_ = [
+  new RegExp('taiwan\\s*,?\\s*province\\s*of\\s*china', 'i'),
+  new RegExp('taiwan\\s*,?\\s*prc', 'i'),
+  new RegExp('taiwan\\s*,?\\s*china', 'i'),
+  new RegExp('china\\s*,?\\s*taiwan', 'i')
 ];
-parser.matchOffending = function(text) {
-  var lower = text.toLowerCase();
-  for (var x = 0; x < parser.patterns.length; ++x) {
-    var re = parser.patterns[x];
-    if (lower.match(re)) {
+parser.matchOffending_ = function(text) {
+  for (var x = 0; x < parser.patterns_.length; ++x) {
+    var re = parser.patterns_[x];
+    if (text.match(re)) {
       return text;
     }
   }
   return false;
-};
-parser.getOffendingNode = function() {
-  var taiwanNode = parser.getTaiwanNode();
-  if (taiwanNode.length == 0) {
-    return null;
-  }
-  if (parser.matchOffending(parser.getText(taiwanNode))) {
-    return taiwanNode;
-  }
-  return null;
 };
 
 $(function() {
